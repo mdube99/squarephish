81af9391c0d6e5deb814a169920f9b9ace25deff
Mark Dube <mjdube99@gmial.com>
2024-04-10 20:23:16 -0500

    added aes encryption

---
+++ squarephish/modules/server/encrypt.py
@@ -0,0 +1,12 @@
+from Crypto.Cipher import AES
+
+def encrypt_aes(msg, secretKey) -> tuple[bytes, bytes, bytes]:
+    aesCipher = AES.new(secretKey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+def decrypt_aes(encryptedMsg, secretKey) -> bytes:
+    (ciphertext, nonce, authTag) = encryptedMsg
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
